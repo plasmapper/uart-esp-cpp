34ed9f183ca550b50425b5223ced948e84b7c9b3
plasmapper <99073058+plasmapper@users.noreply.github.com>
2022-11-22 03:26:37 +0400

    Specified enum values

--- component/include/pl_uart_types.h
+++ component/include/pl_uart_types.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "stdint.h"
 
 //==============================================================================
 
@@ -7,35 +8,35 @@ namespace PL {
 //==============================================================================
 
 /// @brief UART parity
-enum class UartParity {
+enum class UartParity : uint8_t {
   /// @brief no parity bit
-  none,
+  none = 0,
   /// @brief even parity bit
-  even,
+  even = 1,
   /// @brief odd parity bit
-  odd
+  odd = 2
 };
 
 /// @brief Number of UART stop bits
-enum class UartStopBits {
+enum class UartStopBits : uint8_t {
   /// @brief 1 stop bit
-  one,
+  one = 0,
   /// @brief 1.5 stop bits
-  onePointFive,
+  onePointFive = 1,
   /// @brief 2 stop bits
-  two
+  two = 2
 };
 
 /// @brief UART flow control type
-enum class UartFlowControl {
+enum class UartFlowControl : uint8_t {
   /// @brief no flow control
-  none,
+  none = 0,
   /// @brief RTS flow control
-  rts,
+  rts = 1,
   /// @brief CTS flow control
-  cts,
+  cts = 2,
   /// @brief RTS and CTS flow control
-  rtsCts
+  rtsCts = 3
 };
 
 //==============================================================================
