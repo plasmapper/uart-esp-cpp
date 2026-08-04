8580f231763ece3347f2413950c59f86645b09a9
plasmapper <99073058+plasmapper@users.noreply.github.com>
2026-08-04 16:19:14 +0200

    Changed lock timeout handling

--- component/pl_uart_base.cpp
+++ component/pl_uart_base.cpp
@@ -50,12 +50,9 @@ Uart::~Uart() {
 
 esp_err_t Uart::Lock(TickType_t timeout) {
   esp_err_t error = mutex.Lock(timeout);
-  if (error == ESP_OK)
-    return ESP_OK;
-  if (error == ESP_ERR_TIMEOUT && timeout == 0)
-    return ESP_ERR_TIMEOUT;
-  ESP_RETURN_ON_ERROR(error, TAG, "mutex lock failed");
-  return ESP_OK;
+  if (error != ESP_OK && (error != ESP_ERR_TIMEOUT || timeout != 0))
+    ESP_LOGE(TAG, "mutex lock failed");
+  return error;
 }
 
 //==============================================================================
